0c2c2ee5939f9ff1d7b1fa13b1212f76bba1357f
diego.jaramillo <diego.jaramillo@globant.com>
2019-01-16 10:36:22 -0500

    cleanup

--- PersitenceService/Config/ServiceRegister.cs
+++ PersitenceService/Config/ServiceRegister.cs
@@ -14,36 +14,11 @@ namespace PersitenceService.Config
     {
         public void Compose(IServiceRegistry serviceRegistry)
         {
-            #region Context
-
-            //var ambientDbContextLocator = new AmbientDbContextLocator();
-
-            //serviceRegistry.Register<IDbContextScopeFactory>((x) => new DbContextScopeFactory(null));
-            //serviceRegistry.Register<IAmbientDbContextLocator, AmbientDbContextLocator>(new PerScopeLifetime());
-
-            #endregion
-
-            #region Repositories
-
-            //serviceRegistry.Register<IRepository<DataFileUploader_FileTypes>>((x) => new Repository<DataFileUploader_FileTypes>(ambientDbContextLocator));
-            //serviceRegistry.Register<IRepository<DataFileUploader_DataTypes>>((x) => new Repository<DataFileUploader_DataTypes>(ambientDbContextLocator));
-            //serviceRegistry.Register<IRepository<DataFileUploader_DataFile>>((x) => new Repository<DataFileUploader_DataFile>(ambientDbContextLocator));
-            //serviceRegistry.Register<IRepository<DataFileUploader_DataFileDefinitions>>((x) => new Repository<DataFileUploader_DataFileDefinitions>(ambientDbContextLocator));
-            //serviceRegistry.Register<IRepository<DataFileUploader_DataFileDefinition_Fields>>((x) => new Repository<DataFileUploader_DataFileDefinition_Fields>(ambientDbContextLocator));
-
-
-            #endregion
-
+             
             #region Services
 
             serviceRegistry.Register<IPresidentService, PresidentService>();
-            //serviceRegistry.Register<IDataTypeService, DataTypeService>();
-            //serviceRegistry.Register<IDataFileService, DataFileService>();
-            //serviceRegistry.Register<IDataFileDefinitionsService, DataFileDefinitionsService>();
-            //serviceRegistry.Register<IDataFileDefinitionFieldService, DataFileDefinitionFieldService>();
-
-
-
+        
             #endregion
         }
     }
